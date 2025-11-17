8d9ecae7255bb1918c44164416501145ca4c3104
xichomea <xichomea@gmail.com>
2025-11-17 22:56:09 +0900

    Particle 클래스 리팩토링

---
+++ Particle.js
@@ -0,0 +1,34 @@
+class Particle {
+  constructor(position) {
+    this.acceleration = createVector(0, 0.05);
+    this.velocity = createVector(random(-1, 1), random(-1, 0));
+    this.position = position.copy();
+    this.lifespan = 255;
+    }
+
+    applyForce(force){
+      this.acceleration.add(force);
+    }
+
+    run() {
+    this.update();
+    this.display();
+    }
+
+    update() {
+      this.velocity.add(this.acceleration);
+      this.position.add(this.velocity);
+      this.lifespan -= 2;
+    }
+
+    display() {
+      stroke(200, this.lifespan);
+      strokeWeight(2);
+      fill(127, this.lifespan);
+      ellipse(this.position.x, this.position.y, 12, 12);
+    }
+
+    isDead() {
+      return this.lifespan < 0;
+    }
+}
