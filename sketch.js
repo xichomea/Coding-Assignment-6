1b5077e054e192ebf6e11def9b4ee28ab6004355
xichomea <xichomea@gmail.com>
2025-11-17 22:40:44 +0900

    기본 파티클 시스템 예제 추가
    addforce() 적용 전

--- sketch.js
+++ sketch.js
@@ -1,7 +1,67 @@
+let system;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(720, 400);
+  system = new ParticleSystem(createVector(width / 2, 50));
 }
 
 function draw() {
-  background(220);
+  background(51);
+
+  system.addParticle();
+  system.run();
+}
+
+// --------------------------------------------------------
+class Particle {
+  constructor(position) {
+    this.acceleration = createVector(0, 0.05);
+    this.velocity = createVector(random(-1, 1), random(-1, 0));
+    this.position = position.copy();
+    this.lifespan = 255;
+    }
+
+    run() {
+    this.update();
+    this.display();
+    }
+
+    update() {
+      this.velocity.add(this.acceleration);
+      this.position.add(this.velocity);
+      this.lifespan -= 2;
+    }
+
+    display() {
+      stroke(200, this.lifespan);
+      strokeWeight(2);
+      fill(127, this.lifespan);
+      ellipse(this.position.x, this.position.y, 12, 12);
+    }
+
+    isDead() {
+      return this.lifespan < 0;
+    }
+}
+
+// --------------------------------------------------------
+class ParticleSystem {
+  constructor(position) {
+    this.origin = position.copy();
+    this.particles = [];
+    }
+
+  addParticle() {
+    this.particles.push(new Particle(this.origin));
+    }
+
+  run() {
+    for (let i = this.particles.length-1; i >= 0; i--) {
+      let p = this.particles[i];
+      p.run();
+      if (p.isDead()) {
+        this.particles.splice(i, 1);
+           }
+        }
+    }
 }
